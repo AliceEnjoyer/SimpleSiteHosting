e7a560f265d4040527626f042b2d8cd4777d587c
AliceEnjoyer <ilin06serhii@gmail.com>
2023-08-16 20:09:47 +0300

    another commit

--- cmd/SimpleServerHosting/pages/scripts/script.js
+++
@@ -1,7 +0,0 @@
-const MsgQAcceptCookies = "This website uses cookies in order to offer you the most relevant information. Please accept cookies for optimal performance."
-
-if(localStorage.getItem("isCookies").toString() == "false" 
-    && confirm(MsgQAcceptCookies)){
-    alert("Now we can offer you the most relevant information!")
-    localStorage.setItem("isCookies", "true")
-}
